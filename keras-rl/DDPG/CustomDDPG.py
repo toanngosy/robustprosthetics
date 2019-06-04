395828641cdfdceb03da950b014a5cf3287db9d2
Berlitos <jb45410@gmail.com>
2019-06-04 22:54:36 +0200

    fix bug symetric

--- keras-rl/DDPG/CustomDDPG.py
+++ keras-rl/DDPG/CustomDDPG.py
@@ -28,16 +28,20 @@ class CustomDDPGAgent(DDPGAgent):
     def transform2symmetric(self, obs, action):
         #Symetrize obs and new_obs
         index_sym_obs = [6, 7, 10, 11, 14, 15, 24, 25, 28, 29]
-        for tab in [obs]:
-            for index in index_sym_obs:
-                tab[index], tab[index+2] = tab[index+2], tab[index]
+
+        new_obs = deepcopy(obs)
+        new_action = deepcopy(action)
+
+        # Symetrize observation
+        for index in index_sym_obs:
+            new_obs[index], new_obs[index+2] = new_obs[index+2], new_obs[index]
 
         # Symetrize action
         index_sym_action = [i for i in range(9)]
         for index in index_sym_action:
-            action[index], action[index+9] = action[index+9], action[index]
+            new_action[index], new_action[index+9] = new_action[index+9], new_action[index]
 
-        return obs, action
+        return new_obs, new_action
 
 
     def backward(self, reward, terminal=False):
@@ -46,8 +50,17 @@ class CustomDDPGAgent(DDPGAgent):
             self.memory.append(self.recent_observation, self.recent_action, reward, terminal,
                             training=self.training)
 
+
             # We also store its symmetric
             obs_sym, act_sym = self.transform2symmetric(self.recent_observation, self.recent_action)
+            
+            print('**********')
+            print(self.recent_observation)
+            print(obs_sym)
+            print(self.recent_action)
+            print(act_sym)
+            print('**********')
+            
             self.memory.append(obs_sym, act_sym, reward, terminal, training=self.training)
 
 
